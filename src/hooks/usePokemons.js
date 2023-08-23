b88aab4a84f0149c8446eab099e5cea6d2c1046e
Alejandro M <alejandromaimus147@gmail.com>
2023-08-23 13:17:33 +0200

    using useRef to avoid fetch same search multiple times

--- src/hooks/usePokemons.js
+++ src/hooks/usePokemons.js
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchPokemons } from '../services/pokemons.js'
 
 export function usePokemons ({ search }) {
   const [pokemons, setPokemons] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const previousSearch = useRef(search)
 
   const getPokemons = () => {
+    if (previousSearch.current === search) return
+    previousSearch.current = search
+
     setIsLoading(true)
     searchPokemons({ search })
       .then(newPokemons => setPokemons(newPokemons))
